13962f84670d8a1376edc8bfd39abfdc42d25e77
Mike-casper <misha7709123@gmail.com>
2022-07-19 22:34:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ objectB.d = "DEF";
 
 console.log(objectB.a);
 console.log(objectA);
+
+
+const a = function (){
+  console.log("Hey three");
+}
+a();
+
+const b = () => {
+  console.log("Hey, ES6")
+}
+b();
+
