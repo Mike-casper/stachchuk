570544df1bff422929b51962a37d7b383295124d
Mike-casper <misha7709123@gmail.com>
2022-07-20 23:36:59 +0300

    Create methodFunction.js

---
+++ methodFunction.js
@@ -0,0 +1,18 @@
+
+const myCity = {
+  city:"New York",
+  cityGreeting:function(){
+    console.log("Hey, there!")
+  }
+}
+
+const myCountry = {
+  country: "USA",
+  countryGreeting(){
+    console.log("Greeeting");
+  }
+}
+
+
+myCity.cityGreeting();
+myCountry.countryGreeting();
