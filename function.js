eb8cdb46070313204a96469305b0cabe2e232d28
Mike-casper <misha7709123@gmail.com>
2022-07-21 22:37:53 +0300

    Create function.js

---
+++ function.js
@@ -0,0 +1,38 @@
+
+let a = 5
+let b = 4
+
+function sum(a,b){
+  c = a + b;
+  console.log(c);
+}
+
+sum(a,b)
+a = 4, b = 9;
+sum(a,b)
+sum(5, 10);
+
+function myFn (a,b){
+  let c
+  a = a + 1;
+  c = a + b;
+  return c
+}
+console.log(myFn(10,15));
+
+
+const mome = {
+  name: "Bob",
+  age:25
+}
+
+function increaseAge(age){
+mome.age += 1
+return age
+}
+
+increaseAge(mome.age)
+console.log(mome);
+
+
+
