db414cdb5189c8c8cfe989ad98bc6122c73bc080
Mike-casper <misha7709123@gmail.com>
2022-07-22 22:11:41 +0300

    Update function.js

--- function.js
+++ function.js
@@ -1,38 +1,67 @@
 
-let a = 5
-let b = 4
-
-function sum(a,b){
-  c = a + b;
-  console.log(c);
-}
-
-sum(a,b)
-a = 4, b = 9;
-sum(a,b)
-sum(5, 10);
-
-function myFn (a,b){
-  let c
-  a = a + 1;
-  c = a + b;
-  return c
-}
-console.log(myFn(10,15));
-
-
 const mome = {
   name: "Bob",
   age:25
 }
-
 function increaseAge(age){
 mome.age += 1
 return age
 }
-
 increaseAge(mome.age)
 console.log(mome);
 
+const profile ={
+  name: "Popka",
+  age: 29
+}
+function upProfile(profile2){
+const upProfile = Object.assign({}, profile2);
+upProfile.age +=1
+return upProfile;
+}
+const profileTwo = upProfile(profile);
+console.log(profileTwo);
+console.log(profile);
 
+// Call back function
+
+function printMyName(){
+  console.log("Mikejjo")
+}
+setTimeout(printMyName, 2000);
+
+//
+
+let a,b;
+
+function myFN(){
+  let b
+  a = 10;
+  b = true;
+  console.log(b);
+}
+myFN();
+console.log(a);
+console.log(b);
+
+
+//-----//
+const c = 6;
+function myFnn(){
+  function innerFN(){
+    console.log(c)
+  }
+  innerFN();
+}
+myFnn();
+
+
+//-----// don't use it
+function example(){
+  q = 17;
+  console.log(q);
+}
+example();
+console.log(q);
+//-----//
 
